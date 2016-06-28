b19a7daf8c98ff5f42218e9c82b22e75ce3e17fe
elisashanghai <elisa.shanghai@gmail.com>
2016-06-28 16:08:35 -0700

    Part 1, 17. finished
     Implementing the Like Feature

---
+++ ViewControllers/TimelineViewController.swift
@@ -0,0 +1,77 @@
+//
+//  TimelineViewController.swift
+//  Makestagram
+//
+//  Created by Yang Song on 6/26/16.
+//  Copyright © 2016 Make School. All rights reserved.
+//
+
+import UIKit
+import Parse
+
+class TimelineViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    
+    var photoTakingHelper: PhotoTakingHelper?
+    var posts: [Post] = []
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        self.tabBarController?.delegate = self
+    }
+    
+    func takePhoto() {
+        // instantiate photo taking class, provide callback for when photo is selected
+        photoTakingHelper = PhotoTakingHelper(viewController: self.tabBarController!) { (image: UIImage?) in
+            let post = Post()
+            // 1
+            post.image.value = image!
+            post.uploadPost()
+        }
+    }
+    
+    override func viewDidAppear(animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        ParseHelper.timelineRequestForCurrentUser { (result: [PFObject]?, error: NSError?) -> Void in
+            self.posts = result as? [Post] ?? []
+            
+            self.tableView.reloadData()
+        }
+    }
+}
+// MARK: Tab Bar Delegate
+
+extension TimelineViewController: UITabBarControllerDelegate {
+    
+    func tabBarController(tabBarController: UITabBarController, shouldSelectViewController viewController: UIViewController) -> Bool {
+        if (viewController is PhotoViewController) {
+            takePhoto()
+            return false
+        } else {
+            return true
+        }
+    }
+}
+
+extension TimelineViewController: UITableViewDataSource {
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // 1
+        return posts.count
+    }
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+
+        let cell = tableView.dequeueReusableCellWithIdentifier("PostCell") as! PostTableViewCell
+        
+        let post = posts[indexPath.row]
+        post.downloadImage()
+        post.fetchLikes()
+        cell.post = post
+        
+        return cell
+    }
+}
